101a7490f05be1b93afe777e52874c38d19d4b76
dnnghia2624 <dnghia9119@gmail.com>
2025-10-16 23:40:44 +0700

    update

--- client/src/components/examples/ExpenseFormExample.tsx
+++
@@ -1,9 +0,0 @@
-import { ExpenseForm } from '../ExpenseForm';
-
-export default function ExpenseFormExample() {
-  return (
-    <div className="max-w-md mx-auto p-4">
-      <ExpenseForm onSubmit={(expense) => console.log('Expense submitted:', expense)} />
-    </div>
-  );
-}
